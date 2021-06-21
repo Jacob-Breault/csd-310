69afc551d7295e3f343477d95778786bada139d7
Jacob-Breault <85079832+Jacob-Breault@users.noreply.github.com>
2021-06-20 21:03:02 -0500

    Update pytech_queries.py

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,1 +1,39 @@
+#Jacob Breault
+#6/20/2021
+#Module 5.3 Assignment
+#Purpose: How to connect test
+
+from pymongo import MongoClient
+
+#connection string.
+url = "mongodb+srv://admin:admin@cluster0.kseok.mongodb.net/pytech?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
+
+# connect to the MongoDB cluster 
+client = MongoClient(url)
+
+# connect pytech database
+db = client.pytech
+
+# get the students collection 
+students = db.students
+
+# find all students in the collection 
+student_list = students.find({})
+
+# display message 
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+# loop over the collection and output the results 
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+# find document by student_id
+bilbo = students.find_one({"student_id": "1008"})
+
+# output
+print("\n  -- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --")
+print("  Student ID: " + bilbo["student_id"] + "\n  First Name: " + bilbo["first_name"] + "\n  Last Name: " + bilbo["last_name"] + "\n")
+
+# exit message 
+input("\n\n  End of program, press any key to continue...")
 
